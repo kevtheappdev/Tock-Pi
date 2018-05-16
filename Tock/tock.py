677c821c818cbaed536dffc3a9f1b0575df92d24
kevtheappdev <kevin.turnerapps@gmail.com>
2018-05-16 08:46:34 -0700

    Initial commmit. Functioning HomeScreen with NASA image of the day and weather displayed.

---
+++ Tock/tock.py
@@ -0,0 +1,77 @@
+import kivy
+kivy.require('1.10.1') # replace with your current kivy version !
+
+from kivy.clock import Clock
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.core.window import Window
+from kivy.graphics import Rectangle
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.image import AsyncImage
+from kivy.lang.builder import Builder
+
+# Builder.load_string('''<Widget>:
+#     canvas.after:
+#         Line:
+#             rectangle: self.x+1,self.y+1,self.width-1,self.height-1
+#             dash_offset: 5
+#             # dash_length: 3''')
+
+from widgets import *
+
+
+class HomeScreen(FloatLayout):
+    def __init__(self, **kwargs):
+        super(HomeScreen, self).__init__(**kwargs)
+
+        # TODO: load from config
+
+        # view initialization
+        backdrop = AnchorLayout(anchor_x='center', anchor_y='center')
+        self.image = AsyncImage()
+        backdrop.add_widget(self.image)
+
+        bottom_center = AnchorLayout(anchor_x='center', anchor_y='bottom')
+        self.title_label = Label(size_hint=(None, None))
+        bottom_center.add_widget(self.title_label)
+
+        center = AnchorLayout(anchor_x='center', anchor_y='center')
+        self.clock = TockClock()
+        center.add_widget(self.clock)
+
+        top_right = AnchorLayout(anchor_x='left', anchor_y='top')
+        self.weather_label = WeatherLabel(size_hint=(0.1, 0.1))
+        top_right.add_widget(self.weather_label)
+
+        self.add_widget(backdrop)
+        self.add_widget(bottom_center)
+        self.add_widget(center)
+        self.add_widget(top_right)
+
+        self.image_request()
+        Clock.schedule_interval(self.image_request, 86400)
+
+    def image_request(self, val=0):
+        # TODO make this async, section off the url
+        req = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        data = req.json()
+        if 'error' in data:
+            print('ERROR: Could not load: {}'.format(data['error']))
+            # TODO: Implement logging and have some kind of fall back
+        if 'hdurl' in data:
+            print('image url: {}'.format(data['hdurl']))
+            self.image.source = data['hdurl']
+        if 'title' in data:
+            self.title_label.text = data['title']
+
+class Tock(App):
+    def build(self):
+        return HomeScreen()
+
+
+if __name__ == '__main__':
+    Window.size = (1366, 768)
+    # Window.fullscreen = True
+    Tock().run()
---
+++ Tock/widgets.py
@@ -0,0 +1,60 @@
+import time
+import json
+import requests
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.uix.image import AsyncImage
+from kivy.graphics import Color
+from kivy.graphics import Rectangle
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.layout import Layout
+
+
+class WeatherLabel(Label):
+    def __init__(self, **kwargs):
+        super(WeatherLabel, self).__init__(**kwargs)
+        self.fetch()
+        Clock.schedule_interval(self.fetch, 60 * 60)
+        self.font_size = 50
+
+    def fetch(self, val=0):
+        # TODO: Avoid hard coding
+        req = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Cupertino&appid=1c51e68c4823e92a75f2590404fd6634&units=imperial")
+        data = req.json()
+        print(data)
+        if 'error' in data:
+            print('ERROR: Could not load weather data: {}'.format(data))
+
+        # TODO: fix this is gacky
+        temp = int(data['main']['temp'])
+        print(temp)
+        self.text = '{} °'.format(temp)
+
+
+class TockClock(Label):
+    def __init__(self, seconds=False, military=False, **kwargs):
+        super(TockClock, self).__init__(**kwargs)
+        self.seconds = seconds
+        self.military = military
+        self.font_size = 150
+        self.text = self.time_str()
+        Clock.schedule_interval(self.update, 0.5)
+
+    def update(self, val):
+        time_data = self.time_str()
+        self.text = time.strftime(time_data)
+
+    def time_str(self):
+
+        if self.military:
+            t_str = '%H:%M'
+        else:
+            t_str = '%-I:%M %p'
+
+        if self.seconds:
+            return t_str + ':%S'
+        else:
+            return t_str
+
